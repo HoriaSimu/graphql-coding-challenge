5ae07033a7e58a40bb77b350b2ae4b9964aef6ba
Gherasimuru-san <19626576+gherazaurul@users.noreply.github.com>
2021-04-02 14:45:17 +0300

    Added query to fakerql API

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client'
+
+const FETCH_DATA = gql`
+  query GetPosts {
+      allPosts (count: 500) {
+        id
+        createdAt
+      }
+    }
+`;
 
 const App = () => {
+  const { loading, error, data } = useQuery(FETCH_DATA);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return `Error! ${error.message}`;
+
+  const fetchedData = data.allPosts.map(({ id, createdAt }) => {
+    let convertedDate = new Date(Number(createdAt));
+    let year = convertedDate.getFullYear();
+    return (
+      <div key={id}>
+        <p>
+          {id}: {createdAt} ( {year} )
+        </p>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h2>Monthly postcount - 2019</h2>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, gql, ApolloProvider } from '@apollo/client';
 
 import App from './components/App';
 
+const client = new ApolloClient({
+  uri: 'https://fakerql.stephix.uk/graphql',
+  cache: new InMemoryCache()
+});
+
+const providerWrap = (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
 ReactDOM.render(
-  <App />,
+  providerWrap,
   document.querySelector('#root')
 );
