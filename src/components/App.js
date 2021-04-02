5435f41eb4cbd99f46e680a0c0a77bc7ca8e2a0d
Gherasimuru-san <19626576+gherazaurul@users.noreply.github.com>
2021-04-03 01:41:42 +0300

    Added query processing code

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,7 @@ import Chart from './Chart';
 
 const FETCH_DATA = gql`
   query GetPosts {
-      allPosts (count: 500) {
-        id
+      allPosts (count: 50) {
         createdAt
       }
     }
@@ -15,27 +14,58 @@ const FETCH_DATA = gql`
 const App = () => {
   const { loading, error, data } = useQuery(FETCH_DATA);
 
-  //if (loading) return <p>Loading...</p>
-  //if (error) return `Error! ${error.message}`;
+  if (loading) return <p>Loading...</p>
+  if (error) return `Error! ${error.message}`;
 
-  /*
-    // this is a test code to check if data is fetched succesfully from the fakerQL api
-    const fetchedData = data.allPosts.map(({ id, createdAt }) => {
+  // the array `results` will hold the year and month of each post
+  const results = [];
+
+  // usually a .map method should return a value, so this is not ideal
+  // but works for now
+
+  data.allPosts.map(({ createdAt }) => {
     let convertedDate = new Date(Number(createdAt));
     let year = convertedDate.getFullYear();
-    return (
-      <div key={id}>
-        <p>
-          {id}: {createdAt} ( {year} )
-        </p>
-      </div>
+    let month = convertedDate.getMonth();
+
+    results.push({
+      year: year,
+      month: month
+    });
+  });
+
+  // filtering away the posts not made in 2019
+  // this might be unnecessary, since all posts from fakerQL *seem* to be made in 2019
+  // but better safe than sorry
+
+  const filteredResults = results.filter((post) => {
+    return post.year === 2019;
+  });
+
+  // `chartData` will contain the post count for each month
+  // this will be passed as a prop to the Chart component
+
+  const chartData = [];
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+
+  const count = (results, value) => {
+    return results.filter(item => item.month === value).length;
+  }
+
+  for (let i = 0; i <= 11; i++) {
+    chartData.push(
+      {
+        month: monthNames[i],
+        postCount: count(filteredResults, i)
+      }
     );
-  });*/
+  }
 
   return (
     <div className="wrapper">
       <h2>Monthly postcount - 2019</h2>
-      <Chart />
+      <Chart chartData={chartData} />
     </div>
   );
 }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,93 +1,47 @@
+// most of this code is taken directly from a basic vx example
+
 import React from 'react';
 import { Group } from '@visx/group';
 import { Bar } from '@visx/shape';
 import { AxisBottom, AxisLeft } from '@vx/axis';
 import { scaleLinear, scaleBand } from '@visx/scale';
 
-const data = [
-  {
-    month: "January",
-    postCount: 10
-  },
-  {
-    month: "February",
-    postCount: 30
-  },
-  {
-    month: "March",
-    postCount: 40
-  },
-  {
-    month: "April",
-    postCount: 15
-  },
-  {
-    month: "May",
-    postCount: 30
-  },
-  {
-    month: "June",
-    postCount: 30
-  },
-  {
-    month: "July",
-    postCount: 30
-  },
-  {
-    month: "August",
-    postCount: 30
-  },
-  {
-    month: "September",
-    postCount: 24
-  },
-  {
-    month: "October",
-    postCount: 30
-  },
-  {
-    month: "November",
-    postCount: 20
-  },
-  {
-    month: "December",
-    postCount: 10
-  }
-];
-
-// Define the graph dimensions and margins
-const width = 750;
-const height = 500;
-const margin = { top: 20, bottom: 20, left: 20, right: 20 };
-
-// Then we'll create some bounds
-const xMax = width - margin.left - margin.right;
-const yMax = height - margin.top - margin.bottom;
-
-// We'll make some helpers to get at the data we want
-const x = d => d.month;
-const y = d => d.postCount;
-
-// And then scale the graph by our data
-const xScale = scaleBand({
-  range: [0, xMax],
-  round: true,
-  domain: data.map(x),
-  padding: 0.4,
-});
-const yScale = scaleLinear({
-  range: [yMax, 0],
-  round: true,
-  domain: [0, Math.max(...data.map(y))],
-});
-
-// Compose together the scale and accessor functions to get point functions
-const compose = (scale, accessor) => data => scale(accessor(data));
-const xPoint = compose(xScale, x);
-const yPoint = compose(yScale, y);
 
 // Finally we'll embed it all in an SVG
 function Chart(props) {
+  const data = props.chartData;
+
+  // Define the graph dimensions and margins
+  const width = 750;
+  const height = 500;
+  const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+
+  // Then we'll create some bounds
+  const xMax = width - margin.left - margin.right;
+  const yMax = height - margin.top - margin.bottom;
+
+  // We'll make some helpers to get at the data we want
+  const x = d => d.month;
+  const y = d => d.postCount;
+
+  // And then scale the graph by our data
+  const xScale = scaleBand({
+    range: [0, xMax],
+    round: true,
+    domain: data.map(x),
+    padding: 0.4,
+  });
+  const yScale = scaleLinear({
+    range: [yMax, 0],
+    round: true,
+    domain: [0, Math.max(...data.map(y))],
+  });
+
+  // Compose together the scale and accessor functions to get point functions
+  const compose = (scale, accessor) => data => scale(accessor(data));
+  const xPoint = compose(xScale, x);
+  const yPoint = compose(yScale, y);
+
   return (
     <svg width={width} height={height}>
       {data.map((d, i) => {
